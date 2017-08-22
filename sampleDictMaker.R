a33b8875f50011c08e1cab5b69e3e787ab567c3a
alpertSnow <rmddxx@163.com>
2017-08-23 01:04:55 +0800

    Read x,y,z vectors, form a 3d point matrix as the sample points. Merge with the sampleDict-head and -tail, yield sampleDict.

---
+++ sampleDictMaker.R
@@ -0,0 +1,25 @@
+### read x,y,z coordinate vectors
+### form a 3-d point matrix as the sample points
+### merge with the head and tail of the sampleDict file.
+
+# Input coordinate vectors
+x <- seq(1,5,2)
+y <- seq(2,6,2)
+z <- seq(1,1,1)
+
+# Make the point coordiates as "(x y z)"
+points <- expand.grid( x , y , z )
+points.str <- do.call(paste, points)
+coords.str <- paste0('                (', points.str, ')')
+
+# read head and tail
+head <- readLines('sampleDict-head')
+tail <- readLines('sampleDict-tail')
+
+# set fieldNames
+# waiting to be done.......
+
+# write sampleDict
+write(head, 'sampleDict')
+write(coords.str, 'sampleDict', append = TRUE)
+write(tail, 'sampleDict', append = TRUE)
